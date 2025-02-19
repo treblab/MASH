b10f89f3a8419f1e141e3dc8e2e9c28c16df7ebd
treblab <albertnguyen2002@hotmail.com>
2025-02-19 15:35:45 +1100

    Added Soldier sprites and collsion logic to scene

--- Assets/Scripts/Helicopter/Controller.cs
+++ Assets/Scripts/Helicopter/Controller.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    [SerializeField] private GameObject helicopter;
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private Rigidbody2D rb;
 
     private Vector2 movement;
 
-    // Update is called once per frame
+
+    private void Start()
+    {
+        
+    }
+
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -21,5 +28,16 @@ public class Controller : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Handle collisions here
+        Debug.Log("Collided with: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
     }
 }
