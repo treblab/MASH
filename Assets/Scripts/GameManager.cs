f6a73f19324a36ef6b65694118aa87995c9b0cf7
treblab <albertnguyen2002@hotmail.com>
2025-02-21 18:25:54 +1100

    Imported TextMeshProAssets to prepare for UI creation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    [SerializeField] public Canvas canvas;
+    [SerializeField] public Text rescuedText;
+    [SerializeField] public Text inHelicopterText;
+    [SerializeField] public GameObject gameOverScreen;
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Helicopter/Controller.cs
+++ Assets/Scripts/Helicopter/Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
@@ -9,13 +10,6 @@ public class Controller : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
 
     private Vector2 movement;
-
-
-    private void Start()
-    {
-        
-    }
-
 
     void Update()
     {
@@ -28,12 +22,27 @@ public class Controller : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (movement.x < 0)
+        {
+            helicopter.transform.rotation = Quaternion.Euler(0, 180, 0); // Rotate to face left
+        }
+        else if (movement.x > 0)
+        {
+            helicopter.transform.rotation = Quaternion.Euler(0, 0, 0); // Rotate to face right
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Handle collisions here
         Debug.Log("Collided with: " + collision.gameObject.name);
+
+        if (collision.gameObject.name == "Trees")
+        {
+            // Restart Level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
